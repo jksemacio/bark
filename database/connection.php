d8c5d2bb8cb04c7bda6ac34a0ce7976e5c7b625d
Jan Semacio <gsbirth@gmail.com>
2018-08-01 23:06:49 +0800

    add connection for mysql database

---
+++ database/connection.php
@@ -0,0 +1,11 @@
+<?php
+  $dsn = 'mysql: host=localhost; dbname=bark';
+  $user = 'root';
+  $pass = '';
+
+  try {
+    $pdo = new PDO($dsn, $user, $pass);
+  } catch (PDOException $e) {
+    echo 'Connection error ' . $e->message();
+  }
+?>
