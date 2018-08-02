2ef656702d32b1259dab215dba3ce2740c467c13
Jan Semacio <gsbirth@gmail.com>
2018-08-02 08:02:35 +0800

    created login and signup forms

---
+++ includes/login-form.php
@@ -0,0 +1,10 @@
+<?php ?>
+<form method="post">
+  <h3>Login</h3>
+  <ul>
+    <li><input type="text" name="email" placeholder="Email"></li>
+    <li><input type="password" name="password" placeholder="Password"></li>
+    <li><input type="submit" name="login" value="Login"></li>
+    <li><input type="checkbox" value="remember" >Remember me</li>
+  </ul>
+</form>
---
+++ includes/signup-form.php
@@ -0,0 +1,11 @@
+<?php ?>
+<form method="post">
+  <h3>Signup</h3>
+  <ul>
+    <li><input type="text" name="screen_name" placeholder="Full Name"></li>
+    <li><input type="text" name="email" placeholder="Email"></li>
+    <li><input type="password" name="password" placeholder="Password"></li>
+    <li><input type="submit" name="signup" value="Signup for Bark"></li>
+  </ul>
+  <span></span>
+</form>
