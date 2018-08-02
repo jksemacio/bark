32bf78ab1275f403eb1c2ff63c29169aa2a2c3e9
Jan Semacio <gsbirth@gmail.com>
2018-08-02 11:17:32 +0800

    display user data and logout feature

--- classes/user.php
+++ classes/user.php
@@ -32,5 +32,21 @@ class User {
       return false;
     }
   }
+
+  public function user_data($user_id) {
+    $sql = "SELECT * FROM `users` WHERE `user_id` = :user_id";
+    $stmt = $this->pdo->prepare($sql);
+    $stmt->bindParam(":user_id", $user_id, PDO::PARAM_INT);
+    $stmt->execute();
+
+    $user = $stmt->fetch(PDO::FETCH_OBJ);
+    return $user;
+  }
+
+  public function logout() {
+    $_SESSION = array();
+    session_destroy();
+    header('Location: index.php');
+  }
 }
 ?>
--- home.php
+++ home.php
@@ -1,4 +1,12 @@
 <?php
   include 'init.php';
-  echo $_SESSION['user_id'];  
+  $user_id = $_SESSION['user_id'];
+  $user = $u->user_data($user_id);
+
+  echo "Username: " . $user->username . "<br>";
+  echo "Email: " . $user->email . "<br>";
+  echo "Full Name: " . $user->screen_name . "<br>";
+  echo "Following: " . $user->following . "<br>";
+  echo "Followers: " . $user->followers . "<br>";
 ?>
+<a href="logout.php">Log out</a>
