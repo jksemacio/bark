2b7eb6e9bb0cd82a917d18d41e52c686a550b431
Jan Semacio <gsbirth@gmail.com>
2018-08-02 08:01:43 +0800

    include login and signup forms

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
   include 'init.php';
-
-
 ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Bark!</title>
+  </head>
+  <body>
+    <?php include 'includes/login-form.php' ?>
+    <?php include 'includes/signup-form.php' ?>
+  </body>
+</html>
