d1158fa95f0075642ea92e2ba2970c22d958c558
Jan Semacio <gsbirth@gmail.com>
2018-08-02 09:00:41 +0800

    added login function

--- classes/user.php
+++ classes/user.php
@@ -5,5 +5,32 @@ class User {
   function __construct($pdo) {
     $this->pdo = $pdo;
   }
+
+  public function check_input($input) {
+    $input = htmlspecialchars($input);
+    $input = trim($input);
+    $input = stripcslashes($input);
+    return $input;
+  }
+
+  public function login($email, $password) {
+    $password = md5($password);
+    $sql = "SELECT `user_id` FROM `users` WHERE `email` = :email AND `password` = :password";
+    $stmt = $this->pdo->prepare($sql);
+    $stmt->bindParam(":email", $email, PDO::PARAM_STR);
+    $stmt->bindParam(":password", $password, PDO::PARAM_STR);
+    $stmt->execute();
+
+    $user = $stmt->fetch(PDO::FETCH_OBJ);
+    $count = $stmt->rowCount();
+
+    if($count > 0) {
+      $_SESSION['user_id'] = $user->user_id;
+      header('Location: home.php');
+    }
+    else {
+      return false;
+    }
+  }
 }
 ?>
--- includes/login-form.php
+++ includes/login-form.php
@@ -1,4 +1,27 @@
-<?php ?>
+<?php
+  if(isset($_POST['login']) && !empty($_POST['login'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    if(!empty($email) or !empty($password)) {
+      $email = $u->check_input($email);
+      $password = $u->check_input($password);
+
+      if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $error = "Invalid format";
+      }
+      else {
+        if($u->login($email, $password) == false) {
+          $error = "The email or password is incorrect";
+        }
+      }
+    }
+    else {
+      $error = "Please enter username and password";
+    }
+  }
+
+?>
 <form method="post">
   <h3>Login</h3>
   <ul>
@@ -7,4 +30,11 @@
     <li><input type="submit" name="login" value="Login"></li>
     <li><input type="checkbox" value="remember" >Remember me</li>
   </ul>
+  <span>
+    <?php
+      if(isset($error)){
+        echo $error;
+      }
+    ?>
+  </span>
 </form>
